eb7d27b2a6d6289f017fe4bc325dbb2b88c616a8
Pascal Havé <hpwxf@haveneer.com>
2022-12-01 21:29:16 +0100

    Remove generated stream
    Not relevant for a quick demo

--- src/test/scala/demo/AggregationIssue.scala
+++ src/test/scala/demo/AggregationIssue.scala
@@ -60,47 +60,10 @@ class AggregationIssue extends AnyFlatSpec {
     computed_df.printSchema()
   }
 
-  "generated stream test" should "be ok" in {
-
-    def generateRow(index: Int) = {
-      val group_id = (index % 2) + 1
-      val ts = Instant
-        .parse("2020-01-01T00:00:00Z")
-        .plus(Duration.ofHours(12 * (index / 2)))
-      val value = (3 - 2 * group_id) * (index / 2)
-      (ts, "Group" + group_id.toString, value)
-    }
-
-    val df = spark.readStream
-      .format("rate")
-      .option("rowsPerSecond", 3)
-      .load()
-      .flatMap(r => Seq(generateRow(r.getLong(1).toInt)))
-      .toDF(columns: _*)
-
-    val computed_df = df
-      .withWatermark("timestamp", "1 day")
-    //      .groupBy(window($"timestamp", "1 day"), $"group")
-    //      .agg(functions.sum('value).as("agg"))
-
-    computed_df.printSchema()
-
-    computed_df.writeStream
-      .option("truncate", value = false)
-      .format("console")
-      //      .outputMode("complete")
-      .outputMode("update")
-      .option("checkpointLocation", "file_sink/checkpoints")
-      .start()
-      //            .processAllAvailable()
-      .awaitTermination(5 * 1000)
-  }
-
   "memory stream test" should "be ok" in {
 
     implicit val sqlCtx: SQLContext = spark.sqlContext
     val memoryStream = MemoryStream[(Instant, String, Int)]
-    // memoryStream.addData(Seq.range(0, 11).map(generateRow))
     memoryStream.addData(data)
     val df = memoryStream
       .toDF()
